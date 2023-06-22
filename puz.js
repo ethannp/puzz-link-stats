6b8236476f199e642078b35de094437fd31feac7
Ethan <ethananhphan@gmail.com>
2023-06-22 18:00:39 +0200

    Initial commit

---
+++ puz.js
@@ -0,0 +1,168 @@
+let totalsolved = 0;
+let totalpuzzles = 0;
+let totalvariantsolved = 0;
+let totalvariantpuzzles = 0;
+let totalgeneratedsolved = 0;
+let totalgeneratedpuzzles = 0;
+let totalvgsolved = 0;
+let totalvgpuzzles = 0;
+let types = {},
+    sorted;
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('token')) {
+    document.getElementById("token").value = urlParams.get("token");
+}
+
+async function load() {
+    let puzs;
+    try {
+        document.getElementById("wrong").style.display = 'block';
+        document.getElementById("statsbtn").style.display = 'none';
+        document.getElementById("wrong").textContent = 'Please wait a moment...';
+        let token = "Bearer " + document.getElementById('token').value.trim().replaceAll(/[^.A-Za-z0-9_\-]/g, "");
+        puzs = (await (await (fetch(`https://puzz.link/db/api/pzvs_user?limit=999999&order=sort_key.asc&tags_filter=cs.%7B%7D&tags_filter=not.cs.%7Bbroken%7D`, {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "authorization": `${token}`,
+                "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "cross-site"
+            },
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": null,
+            "method": "GET",
+            "mode": "cors",
+            "credentials": "include"
+        }))).json());
+
+        for (const pz in puzs) {
+            let p = puzs[pz];
+            if (types[p.type] == undefined) {
+                types[p.type] = {};
+                types[p.type]['total'] = 0;
+                types[p.type]['solved'] = 0;
+                types[p.type]['variantsolved'] = 0;
+                types[p.type]['varianttotal'] = 0;
+                types[p.type]['generatedtotal'] = 0;
+                types[p.type]['generatedsolved'] = 0;
+                types[p.type]['vgtotal'] = 0;
+                types[p.type]['vgsolved'] = 0;
+            }
+            if (p.tags_filter.includes('variant') && p.generated) {
+                if (p.solved) {
+                    types[p.type]['vgsolved'] += 1;
+                    totalvgsolved++;
+                }
+                types[p.type]['vgtotal'] += 1;
+                totalvgpuzzles++;
+            }
+            if (p.tags_filter.includes('variant')) {
+                if (p.solved) {
+                    types[p.type]['variantsolved'] += 1;
+                    totalvariantsolved++;
+                }
+                types[p.type]['varianttotal'] += 1;
+                totalvariantpuzzles++;
+            } else if (p.generated) {
+                if (p.solved) {
+                    types[p.type]['generatedsolved'] += 1;
+                    totalgeneratedsolved++;
+                }
+                types[p.type]['generatedtotal'] += 1;
+                totalgeneratedpuzzles++;
+            } else {
+                if (p.solved) {
+                    types[p.type]['solved'] += 1;
+                    totalsolved++;
+                }
+                types[p.type]['total'] += 1;
+                totalpuzzles++;
+            }
+        }
+
+        sorted = Object.keys(types).sort().reduce((acc, currValue) => {
+            acc[currValue] = types[currValue];
+            return acc;
+        }, {});
+
+        document.getElementById("wrong").textContent = 'Done! (Scroll down)';
+        let body = document.getElementsByTagName("body")[0];
+        let div = document.getElementsByClassName("listing")[0];
+        if (div == undefined) {
+            div = document.createElement("div");
+            div.classList.add("listing");
+        }
+        innerhtml = `<div class="box"><p>Include: 
+        <label><input type="checkbox" name="puzzletype" value="generated" onclick="update()" id="gen">Generated</label>
+        <label><input type="checkbox" name="puzzletype" value="variant" onclick="update()" id="var">Variant</label>
+        </p>
+        
+        <p>Total: <span id="totalsolved">${totalsolved}</span> / <span id="totalpuzzles">${totalpuzzles}</span>
+        <table><tr><th>Genre</th><th>Solves</th><th>Total</th><th>Remaining</th><th>Percent Complete</th></tr>`;
+        for (const type in sorted) {
+            if (sorted[type]['total'] != 0) {
+                innerhtml += `<tr><td style="padding-right: 15px;">${type}</td><td style="padding: 0 15px;" id="${type}solved">${types[type]['solved']}</td><td style="padding: 0 15px;"id="${type}total">${types[type]['total']}</td><td style="padding: 0 15px;"id="${type}remaining">${types[type]['total'] - types[type]['solved']}</td><td style="padding: 0 15px;" id="${type}percent">${types[type]['solved'] == types[type]['total'] ? "💯<span style='color: gray; font-size: 0.6em'>&nbsp;%</span>" : Math.round(10000 * types[type]['solved'] / types[type]['total']) / 100+"<span style='color: gray; font-size: 0.6em'>&nbsp;%</span>"}</td></tr>`;
+            }
+        }
+        innerhtml += "</table></div>"
+        div.innerHTML = innerhtml;
+        body.appendChild(div);
+        window.scrollBy({
+            top: 200,
+            left: 0,
+            behavior: "smooth",
+        });
+    } catch (error) {
+        document.getElementById("wrong").textContent = 'Something went wrong. Double check your token?';
+        document.getElementById("statsbtn").style.display = 'block';
+        console.log(error);
+        return;
+    }
+}
+
+
+function update() {
+    let variant = document.getElementById("var").checked;
+    let generated = document.getElementById("gen").checked;
+    if (variant && generated) {
+        document.getElementById("totalsolved").textContent = totalsolved + totalvariantsolved + totalgeneratedsolved - totalvgsolved;
+        document.getElementById("totalpuzzles").textContent = totalpuzzles + totalvariantpuzzles + totalgeneratedpuzzles - totalvgpuzzles;
+    } else if (variant) {
+        document.getElementById("totalsolved").textContent = totalsolved + totalvariantsolved;
+        document.getElementById("totalpuzzles").textContent = totalpuzzles + totalvariantpuzzles;
+    } else if (generated) {
+        document.getElementById("totalsolved").textContent = totalsolved + totalgeneratedsolved;
+        document.getElementById("totalpuzzles").textContent = totalpuzzles + totalgeneratedpuzzles;
+    } else {
+        document.getElementById("totalsolved").textContent = totalsolved;
+        document.getElementById("totalpuzzles").textContent = totalpuzzles;
+    }
+    for (const type in sorted) {
+        let solved, total, remaining;
+        if (sorted[type]['total'] != 0) {
+            if (variant && generated) {
+                solved = types[type]['solved'] + types[type]['generatedsolved'] + types[type]['variantsolved'] - types[type]['vgsolved'];
+                total = types[type]['total'] + types[type]['generatedtotal'] + types[type]['varianttotal'] - types[type]['vgtotal'];
+            } else if (variant) {
+                solved = types[type]['solved'] + types[type]['variantsolved'];
+                total = types[type]['total'] + types[type]['varianttotal'];
+            } else if (generated) {
+                solved = types[type]['solved'] + types[type]['generatedsolved'];
+                total = types[type]['total'] + types[type]['generatedtotal'];
+            } else {
+                solved = types[type]['solved'];
+                total = types[type]['total'];
+            }
+            remaining = total - solved;
+            document.getElementById(type + "solved").textContent = solved;
+            document.getElementById(type + "total").textContent = total;
+            document.getElementById(type + "remaining").textContent = remaining;
+            document.getElementById(type + "percent").innerHTML = solved == total ? "💯<span style='color: gray; font-size: 0.6em'>&nbsp;%</span>" : Math.round(10000 * solved / total) / 100+"<span style='color: gray; font-size: 0.6em'>&nbsp;%</span>"
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,72 @@
+try {
+    let types = {};
+    let choosing = [];
+    let htmltypes = document.getElementsByClassName("typesel")[0].children;
+    for (const label in htmltypes) {
+        let input = htmltypes[label].children;
+        if (input != undefined) {
+            let typename = htmltypes[label].textContent;
+            if (input[0].checked) {
+                choosing.push(htmltypes[label].textContent);
+            }
+            types[typename] = {};
+            types[typename]['total'] = 0;
+            types[typename]['solved'] = 0;
+        }
+    }
+    let str = "";
+    if (choosing.length !== 0) {
+        let genres = "";
+        for (const pt in choosing) {
+            genres += '"' + choosing[pt] + '"%2C';
+        }
+        genres = genres.slice(0, -3);
+        str = '&type=in.(' + genres + ')';
+    }
+
+    let puzs = (await (await (fetch('https://puzz.link/db/api/pzvs_user?' + str + 'limit=999999&order=sort_key.asc&generated=eq.false&tags_filter=cs.%7B%7D&tags_filter=not.cs.%7Bbroken%7D&tags_filter=not.cs.%7Bvariant%7D', {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "authorization": "Bearer " + localStorage.getItem("token"),
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site"
+        },
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    }))).json());
+
+    for (const pz in puzs) {
+        let p = puzs[pz];
+        if (types[p.type] != undefined) {
+            if (p.solved) {
+                types[p.type]['solved'] += 1;
+            }
+            types[p.type]['total'] += 1;
+        }
+    }
+
+    let body = document.getElementsByTagName("body")[0];
+    let div = document.getElementsByClassName("listing")[0];
+    if (div == undefined) {
+        div = document.createElement("div");
+        div.classList.add("listing");
+    }
+    innerhtml = "<div><a href='https://ethannp.github.io/puzz-link-stats/index.html?token=" + localStorage.getItem("token") + "' target='_blank'>See full stats</a><table><tr><th>Genre</th><th>Solves</th><th>Total</th><th>Remaining</th></tr>";
+    for (const type in types) {
+        if (types[type]['total'] != 0) {
+            innerhtml += '<tr><td style="padding-right: 15px;">' + type.toString() + '</td><td style="padding: 0 15px;">' + types[type]['solved'].toString() + '</td><td style="padding: 0 15px;">' + types[type]['total'].toString() + '</td><td style="padding: 0 15px;">' + (types[type]['total'] - types[type]['solved']).toString() + '</td></tr>';
+        }
+    }
+    innerhtml += "</table></div>"
+    div.innerHTML = innerhtml;
+    div.style = "position: absolute; top: 10px; left: 0.5em; "
+    body.appendChild(div);
+    body.style.marginTop = (10 + div.offsetHeight) + "px";
+} catch (err) {
+    alert('Something went wrong.');
+}
